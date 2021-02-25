214582f586333c18db43680d8ffabb756b37a261
viveksoni100 <viveksoni100@gmail.com>
2021-02-25 16:28:59 +0530

    attemp 1 is faild for single producer and mul consumer

--- #6#scs-dead-letter-queue/spring-cloud-stream-rabbitmq-demo-dead-letter-queue-handler/message-dlq-processor/src/main/java/com/progressivecoder/messagedlqprocessor/MessageDlqProcessorApplication.java
+++ #6#scs-dead-letter-queue/spring-cloud-stream-rabbitmq-demo-dead-letter-queue-handler/message-dlq-processor/src/main/java/com/progressivecoder/messagedlqprocessor/MessageDlqProcessorApplication.java
@@ -11,8 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MessageDlqProcessorApplication {
 
 	private static final String ORIGINAL_SUBSCRIBER_QUEUE_NAME = "messages.messageInputSubscriber";
+	private static final String ORIGINAL_SUBSCRIBER_QUEUE_NAME2 = "messages.messageInputSubscriber2";
 
 	private static final String SUBSCRIBER_DLQ_NAME = ORIGINAL_SUBSCRIBER_QUEUE_NAME + ".dlq";
+	private static final String SUBSCRIBER_DLQ_NAME2 = ORIGINAL_SUBSCRIBER_QUEUE_NAME2 + ".dlq2";
 
 	@Autowired
 	RabbitTemplate rabbitTemplate;
@@ -23,6 +25,12 @@ public class MessageDlqProcessorApplication {
 
 	@RabbitListener(queues = {SUBSCRIBER_DLQ_NAME})
 	public void processFailedMessage(Message failedMessage){
+
+		System.out.println("Message cannot be processed. Write to database: " + failedMessage.toString());
+	}
+
+	@RabbitListener(queues = {SUBSCRIBER_DLQ_NAME2})
+	public void processFailedMessage2(Message failedMessage){
 
 		System.out.println("Message cannot be processed. Write to database: " + failedMessage.toString());
 	}
